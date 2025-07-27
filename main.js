d8054cbf2201edd69632406561a3d3ad4ffc2ff9
Wesley de Barros da Silva Pinto <wesley.barros@estudante.ufjf.br>
2025-07-27 20:34:51 -0300

    main.js criado

---
+++ main.js
@@ -0,0 +1,4 @@
+const olItens = document.querySelector("#itens")
+const qEntradas = document.querySelector("#entrada")
+const bAdicionar = document.querySelector("#Adicionar")
+const bLimpar = document.querySelector("#limpar")
