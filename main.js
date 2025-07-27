655eec38e7d169a7d9947057f880f2cbb30c8156
Wesley de Barros da Silva Pinto <wesley.barros@estudante.ufjf.br>
2025-07-27 20:43:36 -0300

    Atualizando main.js com lista

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+import {getLista} from "./lista.js"
+
 const olItens = document.querySelector("#itens")
 const qEntradas = document.querySelector("#entrada")
 const bAdicionar = document.querySelector("#Adicionar")
 const bLimpar = document.querySelector("#limpar")
+
+atualizarLista();
+
+function atualizalista(){
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for (let i = 0; i < lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
